1760d24de8f7597100ff76fcbf38519a7909d448
Elson Otake <elsonotake@yahoo.com.br>
2022-06-20 22:43:46 -0300

    Update fetch for shows

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import callApiShows from './callApiShows';
+import loadArticles from './loadArticles';
 import './style.css';
+
+const shows = 'https://api.tvmaze.com/shows';
+
+fetch(shows)
+.then(response => response.json())
+.then(data => loadArticles(data));
