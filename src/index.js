bb57cd3dad8b22adc885c1cc60ed2839674d8033
andgarzonmal <andgarzonmal@unal.edu.co>
2022-06-21 18:12:46 -0500

    call functionalities

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import loadArticles from './modules/loadArticles.js';
+import {hidePopUp} from './modules/popUp.js';
+import { getDataById } from './modules/getData.js';
 import './style.css';
 
 const shows = 'https://api.tvmaze.com/shows';
 
 loadArticles();
+
+document.addEventListener('click',(e)=> {
+  hidePopUp(e)
+})
