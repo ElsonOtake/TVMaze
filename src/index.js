14211ae3c8613adf03277f6797e205dd3b193883
Elson Otake <elsonotake@yahoo.com.br>
2022-06-21 19:00:12 -0300

    Update comments about create new app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-// import createNewApp from './createNewApp.js';
 import loadArticles from './modules/loadArticles.js';
 import './style.css';
 
-// if (!localStorage.getItem('tvMazeId')) {
-//   createNewApp();
-// } 
+// For the creation of the key of new app using the Involvement API we use the following call: 
+//
+// fetch(urlNewApp, {
+//   method: 'POST'
+// })
+// .then(result => result.text())
+// .then(text => localStorage.setItem('tvMazeId', JSON.stringify({ id: text })))
+//
+// As an important part of the coding, we decide to record for future references (Use of .text())
 
 loadArticles();
