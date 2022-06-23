608de635a2da5a3655da1800fad86d2ddcf7f4ac
Elson Otake <elsonotake@yahoo.com.br>
2022-06-22 23:07:20 -0300

    Update return to homepage

--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ import loadArticles from './modules/loadArticles.js';
 import { hidePopUp } from './modules/popUp.js';
 import './style.css';
 
+const logo = document.querySelector('header a');
+const mainSection = document.querySelector('main');
+const episodes = document.querySelector('.episodes');
+const footerCounter = document.querySelector('footer span:last-child');
+
 loadArticles();
 
 document.addEventListener('click', (e) => {
   hidePopUp(e);
 });
+
+logo.addEventListener('click', () => {
+  mainSection.classList.remove('hide');
+  footerCounter.classList.remore('hide');
+  episodes.innerHTML = '';
+})
